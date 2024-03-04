364cff5268cbca01e895fde117288633a0322995
adarsh gurjar <adarsh7470827890@gmail.com>
2024-03-04 18:10:31 +0530

    intialiZe project

---
+++ my-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+import { Navbar } from './components'
+
+const App = () => {
+  return (
+    <div>
+          <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ my-project/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+const [ismenu , setismenu] = useState(false);
+const [sticky , setsticky]  = useState(false);
+
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
